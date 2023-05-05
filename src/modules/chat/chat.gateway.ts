32a61ab496d072ab225d0ef448586d0d45b90773
Duy Nguyen 5 <duy.nguyen5@sotatek.com>
2023-05-05 10:47:24 +0700

    123

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -11,40 +11,32 @@ import { Logger } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { Server } from 'ws';
 
-@WebSocketGateway({ namespace: '/chat' })
-export class ChatGateWay
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
-  @WebSocketServer() server: Server;
-
-  private logger: Logger = new Logger('MessageGateway');
-
-  @SubscribeMessage('msgToServer')
-  public handleMessage(client: Socket, payload: any): Promise<WsResponse<any>> {
-    return this.server.to(payload.room).emit('msgToClient', payload);
-  }
-
-  @SubscribeMessage('joinRoom')
-  public joinRoom(client: Socket, room: string): void {
-    client.join(room);
-    client.emit('joinedRoom', room);
-  }
-
-  @SubscribeMessage('leaveRoom')
-  public leaveRoom(client: Socket, room: string): void {
-    client.leave(room);
-    client.emit('leftRoom', room);
-  }
-
-  public afterInit(server: Server): void {
-    return this.logger.log('Init');
-  }
-
-  public handleDisconnect(client: Socket): void {
-    return this.logger.log(`Client disconnected: ${client.id}`);
-  }
-
-  public handleConnection(client: Socket): void {
-    return this.logger.log(`Client connected: ${client.id}`);
-  }
+// @WebSocketGateway({ namespace: '/chat' })
+// implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+export class ChatGateWay {
+  // @WebSocketServer() server: Server;
+  // private logger: Logger = new Logger('MessageGateway');
+  // @SubscribeMessage('msgToServer')
+  // public handleMessage(client: Socket, payload: any): Promise<WsResponse<any>> {
+  //   return this.server.to(payload.room).emit('msgToClient', payload);
+  // }
+  // @SubscribeMessage('joinRoom')
+  // public joinRoom(client: Socket, room: string): void {
+  //   client.join(room);
+  //   client.emit('joinedRoom', room);
+  // }
+  // @SubscribeMessage('leaveRoom')
+  // public leaveRoom(client: Socket, room: string): void {
+  //   client.leave(room);
+  //   client.emit('leftRoom', room);
+  // }
+  // public afterInit(server: Server): void {
+  //   return this.logger.log('Init');
+  // }
+  // public handleDisconnect(client: Socket): void {
+  //   return this.logger.log(`Client disconnected: ${client.id}`);
+  // }
+  // public handleConnection(client: Socket): void {
+  //   return this.logger.log(`Client connected: ${client.id}`);
+  // }
 }
