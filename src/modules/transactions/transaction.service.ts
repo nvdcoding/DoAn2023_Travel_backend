1f89145d88564fb4db63dd014f8a1d3286607d1d
Duy Nguyen 5 <duy.nguyen5@sotatek.com>
2023-05-22 11:14:40 +0700

    1

--- src/modules/transactions/transaction.service.ts
+++ src/modules/transactions/transaction.service.ts
@@ -148,7 +148,7 @@ export class TransactionService {
       .skip((page - 1) * limit)
       .take(limit);
     const [data, countData] = await Promise.all([
-      query.getRawMany(),
+      query.getMany(),
       query.getCount(),
     ]);
     return {
